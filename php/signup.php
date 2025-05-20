543ff01111819d247d3eac7fe3cc7995c515830b
FahrijeGjoka <fahrije.gjokiqi@student.uni-pr.edu>
2025-05-20 14:58:45 +0200

    .

--- php/signup.php
+++ php/signup.php
@@ -108,10 +108,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sign Up</title>
-    <link rel="stylesheet" href="css/signup.css">
+   
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" rel="stylesheet">
-
+ <link rel="stylesheet" href="/CSS/signup.css">
 
     <style>
         .error-message {
