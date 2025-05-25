70615d1eadb4ea86b3682967430695609cd154eb
ElsaKrasniqii <elsa.krasniqi12@student.uni-pr.edu>
2025-05-25 22:15:17 +0200

    ndryshime ne kod

--- gallery.php
+++ gallery.php
@@ -10,16 +10,16 @@
 
 <body>
     <div class="box">
-        <span style="--i:1;"><img src="Yves Saint Laurent.jpg" alt="YSL"></span>
-        <span style="--i:2;"><img src="burberry goddess.webp" alt="Burberry Goddess"></span>
-        <span style="--i:3;"><img src="prrada.webp" alt="Prada"></span>
-        <span style="--i:4;"><img src="chanel.jpg" alt="Chanel"></span>
-        <span style="--i:5;"><img src="fougere.jpg" alt="Fougere"></span>
-        <span style="--i:6;"><img src="tomford.avif" alt="Tom Ford"></span>
-        <span style="--i:7;"><img src="lostcherry.jpg" alt="Tom Ford Lost Cherry"></span>
-        <span style="--i:8;"><img src="dior.webp" alt="Dior"></span>
-        <span style="--i:9;"><img src="gucci.webp" alt="Gucci"></span>
-        <span style="--i:10;"><img src="floral.jpg" alt="Floral"></span>
+        <span style="--i:1;"><img src="images/Yves Saint Laurent.jpg" alt="YSL"></span>
+        <span style="--i:2;"><img src="images/burberry goddess.webp" alt="Burberry Goddess"></span>
+        <span style="--i:3;"><img src="images/prrada.webp" alt="Prada"></span>
+        <span style="--i:4;"><img src="images/chanel.jpg" alt="Chanel"></span>
+        <span style="--i:5;"><img src="images/fougere.jpg" alt="Fougere"></span>
+        <span style="--i:6;"><img src="images/tomford.avif" alt="Tom Ford"></span>
+        <span style="--i:7;"><img src="images/lostcherry.jpg" alt="Tom Ford Lost Cherry"></span>
+        <span style="--i:8;"><img src="images/dior.webp" alt="Dior"></span>
+        <span style="--i:9;"><img src="images/gucci.webp" alt="Gucci"></span>
+        <span style="--i:10;"><img src="images/floral.jpg" alt="Floral"></span>
     </div>
 </body>
 </html>
